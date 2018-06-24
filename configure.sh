e9436a022196add5b0cc4dbb1f724d1c0783ddd0
Paweł Dąbrowski <dziamber@gmail.com>
2018-06-24 17:49:13 +0200

    Update configure.sh and replace layout .html extension with .haml

--- configure.sh
+++ configure.sh
@@ -1,2 +1,2 @@
 sed -i '' -e "s/GddevRailsBoilerplate/$1/g" ./config/application.rb
-sed -i '' -e "s/GddevRailsBoilerplate/$1/g" ./app/views/layouts/application.html.erb
+sed -i '' -e "s/GddevRailsBoilerplate/$1/g" ./app/views/layouts/application.html.haml
