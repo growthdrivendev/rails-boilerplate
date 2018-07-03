cda8f60e653c5d83c4061ecd6fe3cc02dd9f6e7c
Paweł Dąbrowski <dziamber@gmail.com>
2018-07-03 15:27:57 +0200

    Create YML config for the app and different environments

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,10 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module GddevRailsBoilerplate
+  def self.config
+    Rails.application.config_for(:app)
+  end
+
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
